bdd2f3b9d8f6206f5145898020f0f31262bb4973
Paul Hayes <paul.hayes@entropedia.co.uk>
2020-04-07 20:49:57 +0100

    Improved accuracy, added start up autocalibration, calibration button, and kalman filtering@

--- src/main.ino
+++ src/main.ino
@@ -1,5 +1,5 @@
 //#include <HX711.h>
-#define PLOT_MODE false
+#define PLOT_MODE true
 
 #include <Arduino.h>
 #include <AccelStepper.h>
@@ -15,25 +15,17 @@ const int stepperPhaseB = 5;
 const int stepperPhaseC = 6;
 const int stepperPhaseD = 7;
 
-long maxVal;
-long minVal;
-
-double compliantForceThreshold = 1;
-double returnThreshold = 0.5;
+double compliantForceThreshold = 5;
+double returnThreshold = 4.5;
 double center = 0;
 
 int counter = 0;
+int rawValue = 0;
 double val = 0;
 double lowPassFilteredValue = 0;
 double sKalmanFilteredValue = 0;
 
 /*
- This sample code demonstrates how to use the SimpleKalmanFilter object. 
- Use a potentiometer in Analog input A0 as a source for the reference real value.
- Some random noise will be generated over this value and used as a measured value.
- The estimated value obtained from SimpleKalmanFilter should match the real
- reference value.
-
  SimpleKalmanFilter(e_mea, e_est, q);
  e_mea: Measurement Uncertainty 
  e_est: Estimation Uncertainty 
@@ -41,80 +33,84 @@ double sKalmanFilteredValue = 0;
  */
 SimpleKalmanFilter simpleKalmanFilter(2, 2, 0.1);
 
-const int num_register = 8;
-uint8_t inval[num_register];
-
-//HX711 scale;
 AccelStepper stepper = AccelStepper(AccelStepper::HALF4WIRE,stepperPhaseA,stepperPhaseC,stepperPhaseB,stepperPhaseD) ;
 Plotter p;
 
-int scaleToSteps = 300;
+int inputToSteps = 50;
 long destinationPosition = 0;
 long maxTargetPositionDistance = 100;
 const int sendRate = 120;
 const int secondsToPlot = 20;
 long lastSendMillis;
 int filter = 200;
-int adcReadCount = 0;
+long adcReadCount = 0;
 float adcReadRate = 0;
 long move;
 
 ADC *adc = new ADC(); // adc object
 bool newADCValue = false;
-
+bool isCalibrated = false;
 void setup(){
   //scale.begin(loadCellDOutPin,loadCellSckPin,64);
   
   //Serial.begin(115200);
 #if PLOT_MODE
   p.Begin();
-  p.AddTimeGraph( "Some title of a graph", sendRate*10, "kalman filter", sKalmanFilteredValue, "low pass filter",lowPassFilteredValue, "centered", val, "move", move, "adc read rate khz", adcReadRate );
+  p.AddTimeGraph( "Some title of a graph", sendRate*10, "raw input",rawValue, "kalman filter", sKalmanFilteredValue, "low pass filter",lowPassFilteredValue, "centered", val, "threshold", compliantForceThreshold, "adc read rate khz", adcReadRate );
 #else
   Serial.begin(115200);
 #endif
   stepper.setMaxSpeed(1000);
-  stepper.setAcceleration(2000);
-  //center = scale.read_average(4);
-  //stepper.setSpeed(1000);
-  adc->adc1->setAveraging(32); // set number of averages
-  adc->adc1->setResolution(16); // set bits of resolution
-  // it can be any of the ADC_CONVERSION_SPEED enum: VERY_LOW_SPEED, LOW_SPEED, MED_SPEED, HIGH_SPEED_16BITS, HIGH_SPEED or VERY_HIGH_SPEED
-  // see the documentation for more information
-  // additionally the conversion speed can also be ADACK_2_4, ADACK_4_0, ADACK_5_2 and ADACK_6_2,
-  // where the numbers are the frequency of the ADC clock in MHz and are independent on the bus speed.
-  adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::LOW_SPEED); // change the conversion speed
-  // it can be any of the ADC_MED_SPEED enum: VERY_LOW_SPEED, LOW_SPEED, MED_SPEED, HIGH_SPEED or VERY_HIGH_SPEED
-  adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::LOW_SPEED); // change the sampling speed
+  stepper.setAcceleration(4000);
   
+  adc->adc0->setReference(ADC_REFERENCE::REF_1V2);
+  //adc->adc0->enablePGA(0);
+  adc->adc0->setAveraging(32); // set number of averages
+  adc->adc0->setResolution(16); // set bits of resolution
+  adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::MED_SPEED); // change the conversion speed
+  adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED); // change the sampling speed
   
-  adc->adc1->enableInterrupts(adc1_isr);
-  adc->startContinuousDifferential(A10,A11,ADC_1);
-  //adc->adc1->startContinuousDifferential(10,11);
+  adc->adc0->enableInterrupts(adc1_isr);
+  adc->adc0->startContinuousDifferential(A10,A11);
+  
+  //adc->adc0->startContinuousDifferential(10,11);
 
+  pinMode(8,INPUT_PULLUP);
   attachInterrupt(8,onButtonUp,RISING);
 }
 
 void onButtonUp(){
-  center = sKalmanFilteredValue;
+  if(isCalibrated){
+    calibrate();
+  }
+}
+
+void calibrate(){
+  center = lowPassFilteredValue;
+  isCalibrated = true;
 }
 
 void loop(){
-#if not PLOT_MODE
-  if(adc->adc1->fail_flag != ADC_ERROR::CLEAR) {
-    Serial.print("Error ADC0: "); Serial.println(getStringADCError(adc->adc1->fail_flag));
-  }
-#endif
+
 
   if(newADCValue){
     newADCValue = false;
-    int rawValue = -adc->adc1->analogReadContinuous();
+    rawValue = -adc->adc0->analogReadContinuous();
+    #if not PLOT_MODE
+    if(adc->adc0->fail_flag != ADC_ERROR::CLEAR) {
+      Serial.print("Error ADC1: "); Serial.println(getStringADCError(adc->adc0->fail_flag));
+    }
+    #endif
     adcReadCount++;
     lowPassFilteredValue = (rawValue+lowPassFilteredValue*filter) / (filter+1);
     sKalmanFilteredValue = simpleKalmanFilter.updateEstimate(rawValue);
     
     val = distanceFromCenter( sKalmanFilteredValue );
-    
+    if(!isCalibrated && (adcReadCount>3000L)){
+      calibrate();
+    }
 #if PLOT_MODE
+/*
     long currentTime = millis();
     int sendDuration = 1000/sendRate;
     if(sendDuration<=currentTime-lastSendMillis){
@@ -125,15 +121,9 @@ void loop(){
       lastSendMillis = currentTime;
       adcReadCount = 0;
     }
+    */
 #endif
     //Serial.println(val);
-
-
-    if(val<minVal){
-      minVal = val;
-    } else if(val>maxVal) {
-      maxVal = val;
-    }
 
 #if not PLOT_MODE
     if(Serial.available()){
@@ -171,14 +161,35 @@ void loop(){
         Serial.print(compliantForceThreshold);
         Serial.println("");
       }
+      if(command=='v'){
+        compliantForceThreshold++;
+        
+        Serial.print("current value=");
+        Serial.print(val);
+        Serial.print(" low pass value=");
+        Serial.print(lowPassFilteredValue);
+        Serial.println("");
+      }
+      if(command=='>'){
+        compliantForceThreshold++;
+        Serial.println("");
+        Serial.print("scaleToSteps to: ");
+        Serial.print(compliantForceThreshold);
+        Serial.println("");
+      }
     }
 #endif
-    long targetPos = stepper.targetPosition();
-    long currentPos = stepper.currentPosition();
-    if(val>compliantForceThreshold){
+    
+    
+  }
+
+  if(!isCalibrated)
+    return;
+
+  if(val>compliantForceThreshold){
     //stepper.setSpeed(-1000);
 
-      move = (int)round(val)*scaleToSteps;
+      move = (int)round(val)*inputToSteps;
       
       stepper.setSpeed( move );
       stepper.runSpeed();
@@ -186,7 +197,7 @@ void loop(){
     }
     else if(val<-compliantForceThreshold){
 
-      move = (int)round(val)*scaleToSteps;
+      move = (int)round(val)*inputToSteps;
     
       //stepper.moveTo( currentPos + move );
       stepper.setSpeed( move );
@@ -196,11 +207,6 @@ void loop(){
       stepper.moveTo( destinationPosition );
       stepper.run();
     }
-
-    
-  }
-
-  
 }
 
 double distanceFromCenter(double val){
@@ -208,6 +214,6 @@ double distanceFromCenter(double val){
 }
 
 void adc1_isr(void) {
-    adc->adc1->analogReadContinuous();
+    adc->adc0->analogReadContinuous();
     newADCValue = true;
 }
