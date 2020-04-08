7a3976dcc84ebbdaf5003697fe2e15b14f2e9ed4
Paul Hayes <paul.hayes@entropedia.co.uk>
2020-04-08 08:53:48 +0100

    :bulb: added readme. :wastebasket: cleaned up code.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Compliant Stepper Mototr example using a load cell
+
+## Hardware
+* Teensy 3.2 or above
+* cheap load cell
+* cheap unipolar stepper motor with driver board ( 4 with coil inputs )
+* 10nF cap across the loadcell outputs.
+* button ( optional, allows for manually calibration of the load-cell at rest )
+That's it!
+
+## Pins
+
+* A10-A11 loadcell ( yes they are annoying to get to, but at least they aren't on the underside of the board )
+* D4-7 stepper motor contol
+* D8 button ( connected buttons other terminal to ground )
+
+This code utlised the teensy's fast ADC differencial comparator across the wheatstone bridge of the loadcell. 
+
+
+## Why didn't you use the teensy's Programable Gain Amplifier?
+
+I was unable to use the programable gain amplifier of the teensy due to the fact it seems to only work in either 0-1.2v or -1.2-0v. Whereas I wanted to read both positive and negative values across the wheatstone bridge. Although the PGA is adaptive it waits until going above a certain value in the negative threshold before inverting the gain.
--- src/main.ino
+++ src/main.ino
@@ -8,22 +8,10 @@
 #include "Plotter.h"
 #include <SimpleKalmanFilter.h>
 
-const int loadCellDOutPin = 2;
-const int loadCellSckPin = 3;
 const int stepperPhaseA = 4;
 const int stepperPhaseB = 5;
 const int stepperPhaseC = 6;
 const int stepperPhaseD = 7;
-
-double compliantForceThreshold = 5;
-double returnThreshold = 4.5;
-double center = 0;
-
-int counter = 0;
-int rawValue = 0;
-double val = 0;
-double lowPassFilteredValue = 0;
-double sKalmanFilteredValue = 0;
 
 /*
  SimpleKalmanFilter(e_mea, e_est, q);
@@ -36,24 +24,36 @@ SimpleKalmanFilter simpleKalmanFilter(2, 2, 0.1);
 AccelStepper stepper = AccelStepper(AccelStepper::HALF4WIRE,stepperPhaseA,stepperPhaseC,stepperPhaseB,stepperPhaseD) ;
 Plotter p;
 
+ADC *adc = new ADC(); // adc object
+
+
+double compliantForceThreshold = 5;
+double returnThreshold = 4.5;
+double center = 0;
+
+int counter = 0;
+int rawValue = 0;
+double val = 0;
+double lowPassFilteredValue = 0;
+double sKalmanFilteredValue = 0;
+
 int inputToSteps = 50;
 long destinationPosition = 0;
 long maxTargetPositionDistance = 100;
 const int sendRate = 120;
 const int secondsToPlot = 20;
 long lastSendMillis;
+long lastSendADCCount = 0;
 int filter = 200;
 long adcReadCount = 0;
 float adcReadRate = 0;
 long move;
 
-ADC *adc = new ADC(); // adc object
 bool newADCValue = false;
 bool isCalibrated = false;
+
 void setup(){
-  //scale.begin(loadCellDOutPin,loadCellSckPin,64);
-  
-  //Serial.begin(115200);
+
 #if PLOT_MODE
   p.Begin();
   p.AddTimeGraph( "Some title of a graph", sendRate*10, "raw input",rawValue, "kalman filter", sKalmanFilteredValue, "low pass filter",lowPassFilteredValue, "centered", val, "threshold", compliantForceThreshold, "adc read rate khz", adcReadRate );
@@ -64,6 +64,7 @@ void setup(){
   stepper.setAcceleration(4000);
   
   adc->adc0->setReference(ADC_REFERENCE::REF_1V2);
+  // see README.md as to why I don't use this
   //adc->adc0->enablePGA(0);
   adc->adc0->setAveraging(32); // set number of averages
   adc->adc0->setResolution(16); // set bits of resolution
@@ -73,29 +74,31 @@ void setup(){
   adc->adc0->enableInterrupts(adc1_isr);
   adc->adc0->startContinuousDifferential(A10,A11);
   
-  //adc->adc0->startContinuousDifferential(10,11);
-
   pinMode(8,INPUT_PULLUP);
   attachInterrupt(8,onButtonUp,RISING);
 }
 
-void onButtonUp(){
-  if(isCalibrated){
-    calibrate();
-  }
-}
-
-void calibrate(){
-  center = lowPassFilteredValue;
-  isCalibrated = true;
-}
 
 void loop(){
 
-
   if(newADCValue){
     newADCValue = false;
-    rawValue = -adc->adc0->analogReadContinuous();
+    readADC();
+    #if PLOT_MODE
+    plot();
+    #else
+    serialDebug();
+    #endif
+  }
+
+  if(!isCalibrated)
+    return;
+
+  moveMotor();
+}
+
+void readADC(){
+      rawValue = -adc->adc0->analogReadContinuous();
     #if not PLOT_MODE
     if(adc->adc0->fail_flag != ADC_ERROR::CLEAR) {
       Serial.print("Error ADC1: "); Serial.println(getStringADCError(adc->adc0->fail_flag));
@@ -109,42 +112,70 @@ void loop(){
     if(!isCalibrated && (adcReadCount>3000L)){
       calibrate();
     }
-#if PLOT_MODE
-/*
+}
+
+void moveMotor(){
+  if(val>compliantForceThreshold){    
+      move = (int)round(val)*inputToSteps;      
+      stepper.setSpeed( move );
+      stepper.runSpeed();
+    }
+    else if(val<-compliantForceThreshold){
+      move = (int)round(val)*inputToSteps;    
+      stepper.setSpeed( move );
+      stepper.runSpeed();
+    } else if(abs(val)<returnThreshold){
+      move = 0;
+      stepper.moveTo( destinationPosition );
+      stepper.run();
+    }
+}
+
+void calibrate(){
+  center = lowPassFilteredValue;
+  isCalibrated = true;
+}
+
+double distanceFromCenter(double val){
+  return val - center;
+}
+
+void plot(){
     long currentTime = millis();
     int sendDuration = 1000/sendRate;
     if(sendDuration<=currentTime-lastSendMillis){
       
-      adcReadRate = 1.0f*adcReadCount / (currentTime-lastSendMillis);
+      adcReadRate = 1.0f*( adcReadCount-lastSendADCCount ) / (currentTime-lastSendMillis);
   
       p.Plot();
       lastSendMillis = currentTime;
-      adcReadCount = 0;
+      lastSendADCCount = adcReadCount;
     }
-    */
-#endif
-    //Serial.println(val);
 
-#if not PLOT_MODE
+}
+
+void serialDebug(){
+
     if(Serial.available()){
       char command = Serial.read();
       if(command=='z'){
+        Serial.println("");
         stepper.setCurrentPosition(0);
         Serial.println("");
         Serial.println("zeroed position ");
       }
       if(command=='-'){
-        scaleToSteps--;
+        inputToSteps--;
         Serial.println("");
-        Serial.print("scaleToSteps to: ");
-        Serial.print(scaleToSteps);
+        Serial.print("set inputToSteps to: ");
+        Serial.print(inputToSteps);
         Serial.println("");
       }
       if(command=='+'){
-        scaleToSteps++;
+        inputToSteps++;
         Serial.println("");
-        Serial.print("threshold to: ");
-        Serial.print(compliantForceThreshold);
+        Serial.print("set inputToSteps to: ");
+        Serial.print(inputToSteps);
         Serial.println("");
       }
       if(command=='<'){
@@ -157,60 +188,30 @@ void loop(){
       if(command=='>'){
         compliantForceThreshold++;
         Serial.println("");
-        Serial.print("scaleToSteps to: ");
+        Serial.print("threshold to: ");
         Serial.print(compliantForceThreshold);
         Serial.println("");
       }
       if(command=='v'){
         compliantForceThreshold++;
         
+        Serial.println("");
         Serial.print("current value=");
         Serial.print(val);
         Serial.print(" low pass value=");
         Serial.print(lowPassFilteredValue);
         Serial.println("");
       }
-      if(command=='>'){
-        compliantForceThreshold++;
-        Serial.println("");
-        Serial.print("scaleToSteps to: ");
-        Serial.print(compliantForceThreshold);
-        Serial.println("");
-      }
-    }
-#endif
-    
-    
-  }
-
-  if(!isCalibrated)
-    return;
-
-  if(val>compliantForceThreshold){
-    //stepper.setSpeed(-1000);
-
-      move = (int)round(val)*inputToSteps;
       
-      stepper.setSpeed( move );
-      stepper.runSpeed();
-      //stepper.moveTo(currentPos + move );    
-    }
-    else if(val<-compliantForceThreshold){
-
-      move = (int)round(val)*inputToSteps;
-    
-      //stepper.moveTo( currentPos + move );
-      stepper.setSpeed( move );
-      stepper.runSpeed();
-    } else if(abs(val)<returnThreshold){
-      move = 0;
-      stepper.moveTo( destinationPosition );
-      stepper.run();
     }
 }
 
-double distanceFromCenter(double val){
-  return val - center;
+/* Interrupts */
+
+void onButtonUp(){
+  if(isCalibrated){
+    calibrate();
+  }
 }
 
 void adc1_isr(void) {
